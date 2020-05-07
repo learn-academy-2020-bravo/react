95ef6006bdd111871f71d648c03763332f81752b
April Rieger <april.rieger@yahoo.com>
2020-05-07 13:29:22 -0700

    Aprils Drive

---
+++ chantelle-april/src/Content.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Content extends Component{
+  render() {
+    return (
+        <p>Chantelle info...
+        <br />April info...</p>
+    )
+  }
+}
+
+export default Content;
