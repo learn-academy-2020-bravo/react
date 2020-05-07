0f23abf866b863f192b3ea791afae8edb667daec
chrissie-akau <cakau9@gmail.com>
2020-05-07 15:12:27 -0700

    Switching

---
+++ chrissie-nikki-react-state/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Box from './Box.js'
+import './App.css';
+
+class App extends Component{
+  render(){
+    return (
+      <>
+      <Box />
+      <Box />
+      </>
+    )
+    }
+}  
+export default App;
