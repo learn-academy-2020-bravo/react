bfe2ce323b2183f7757f001928be82f491731ede
Austin Walker <austinsrwalker@gmail.com>
2020-05-07 12:53:58 -0700

    git commit attepmt 4

---
+++ Reactapp-jeremy-thomas-austin/sample-app/src/Content.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class Content extends Component {
+  render (){
+    return(
+      <>
+        <h1></h1>
+        <p> Jeremy Thomas & and Austin </p>
+
+      </>
+    )
+  }
+}
+export default Content;
---
+++ Reactapp-jeremy-thomas-austin/sample-app/src/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class Footer extends Component {
+  render (){
+    return(
+      <>
+        <h6> Team Normal Heights</h6>
+      </>
+    )
+  }
+}
+export default Footer;
