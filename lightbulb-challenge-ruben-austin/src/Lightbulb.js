8e1a47ebe605e0b4c10f2eec48bb2680ef0574d6
Ruben Ramirez <rramirez110911@gmail.com>
2020-05-08 10:57:01 -0700

    lightbulb turns on

--- lightbulb-challenge-ruben-austin/src/Lightbulb.js
+++ lightbulb-challenge-ruben-austin/src/Lightbulb.js
@@ -6,28 +6,35 @@ class Lightbulb extends Component {
       super(props)
       this.state = {
 
-        lightArray: ["white", "yellow"]
-      }
-
-    handleChange = () => {
-      let lightVar = [] =>
-
-    //  if [0] => add 1 store value
-    // if [1] => subtract 1 store value
-    return 
-
+        lightArray: ["white", "yellow"],
+        onOff: " "
       }
     }
-  }
+    handleChange = () => {
+    //   let lightVar = () =>
+    //  if [0] => add 1 store value
+    // if [1] => subtract 1 store value
+    // return 
+    //   }
+      let off = this.state.lightArray[0]
+
+      let on = this.state.lightArray[1]
+      if(off == "white") {
+        this.setState({onOff: this.state.lightArray[1]})
+      } else if(on == "yellow") {
+        this.setState({onOff: this.state.lightArray[0]})
+      }
+    }
+
     render () {
       return(
         <>
-          <div onClick= { this.handleChange}>
+          <div onClick= { this.handleChange} class = "box" style = {{backgroundColor: this.state.onOff}}>
 
             <button  className= "off button"> off
             </button>
-            <button  className= "on button"> on
-            </button>
+            {/* <button  className= "on button"> on
+            </button> */}
           </div>
 
         </>
