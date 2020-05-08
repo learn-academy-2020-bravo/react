1769d6e1d7d2f100414b0654d08115b9167921f4
Austin Walker <austinsrwalker@gmail.com>
2020-05-08 09:54:47 -0700

    boilerplate off

---
+++ lightbulb-challenge-ruben-austin/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Lightbulb form './Lightbulb.js'
+
+
+
+
+class App extends Component {
+
+
+  render() {
+    return
+      <>
+        < lightbulb />
+      </>
+  );
+}
+}
+
+export default App;
---
+++ lightbulb-challenge-ruben-austin/src/Lightbulb.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Lightbulb extends Component {
+    constructor (props) {
+      super(props)
+      this.state = {
+
+      }
+    }
+    render () {
+      return(
+        <>
+        </>
+      )
+    }
+
+
+
+
+}
+export default Lightbulb
