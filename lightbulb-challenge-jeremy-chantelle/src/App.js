8c81697225e7bc0b9a001afd232c1fe477a1cb24
Jeremy Gabriel <jeremyagabriel@gmail.com>
2020-05-08 10:22:43 -0700

    First push

---
+++ lightbulb-challenge-jeremy-chantelle/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import LightBox from './LightBox'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return(
+      <>
+        <h1>Light Bulb Challenge</h1>
+        < LightBox />
+      </>
+    )
+  }
+}
+
+export default App;
