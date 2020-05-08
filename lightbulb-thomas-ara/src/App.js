6512e6cbd71ca28309598a8ae6ea414e5b23d070
Thomas Gonda <passinto06@gmail.com>
2020-05-08 15:10:01 -0700

    problem is done correctly now

--- lightbulb-thomas-ara/src/App.js
+++ lightbulb-thomas-ara/src/App.js
@@ -1,43 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import OFFbulb from './images/OFFbulb.jpg'
-import ONbulb from './images/ONbulb.jpg'
-import OFFswitch from './images/Offswitch.jpeg'
-import ONswitch from './images/ONswitch.jpg'
-
+import Lightbulb from './Lightbulb.js'
 
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-        bulbImage: OFFbulb,
-        switchImage: OFFswitch,
         switchArray: []
     }
   }
 
   addSwitch = () => {
-    this.setState({switchArray: [...this.state.switchArray, this.state.switchImage]})
-    console.log(this.state.switchArray)
+    this.setState({switchArray: [...this.state.switchArray, < Lightbulb />]})
   }
 
   deleteSwitch = () =>  {
     this.setState({switchArray: this.state.switchArray.slice(0,this.state.switchArray.length - 1)})
-    console.log(this.state.switchArray)
-  }
-
-  handleChange = () => {
-    if(this.state.bulbImage === ONbulb) {
-      this.setState({bulbImage: OFFbulb})
-      this.setState({switchImage: OFFswitch})
-      this.setState({switchArray: this.state.switchArray.map(x => OFFswitch)})
-    }
-    else {
-      this.setState({bulbImage: ONbulb})
-      this.setState({switchImage: ONswitch})
-      this.setState({switchArray: this.state.switchArray.map(x => ONswitch)})
-    }
   }
 
   render(){
@@ -47,10 +26,7 @@ class App extends Component {
           <button className = "button"  onClick = {this.addSwitch}> Add a Light Switch. </button>
           <button className = "button"  onClick = {this.deleteSwitch}> Remove a Light Switch. </button>
         </div>
-
-        <img src = {this.state.bulbImage}/>
-        <div onClick = {this.handleChange}> {this.state.switchArray.map(x => <img src = {x} height = "100" width = "100"/>)} </div>
-        //<img src = {this.state.switchArray} onClick = {this.handleChange}/>
+        <div> {this.state.switchArray} </div>
       </>
     );
   }
