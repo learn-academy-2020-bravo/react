da4503b6e6355f95d21bdcf96bb2bd124a8fe846
chrissie-akau <cakau9@gmail.com>
2020-05-11 15:10:06 -0700

    Switch Drivers

--- props-chrissie-ruben/src/App.js
+++ props-chrissie-ruben/src/App.js
@@ -1,27 +1,33 @@
 import React, {Component} from 'react';
 import Dice from './components/Dice'
 import './App.css';
+import Rolls from './components/Rolls';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      diceDigits: [1, 2, 3, 4, 5, 6],
-      currentRoll: ""
+      currentRoll: "",
+      previousLog: []
     }
   }
 
   diceRolling = () => {
-    let randomRoll = Math.floor(Math.random() * this.state.diceDigits.length)
-    this.setState({ currentRoll: this.state.diceDigits[randomRoll] })
+    let randomRoll = Math.ceil(Math.random() * 6)
+    this.state.previousLog.push(randomRoll)
+    this.setState({ currentRoll: randomRoll, 
+                    previousLog: this.state.previousLog })
   }
 
   render() {
     return(
       <>
         <Dice
-         diceCurrentRoll ={ this.state.currentRoll}
-         boxClick = {this.diceRolling}
+         diceCurrentRoll ={ this.state.currentRoll }
+         boxClick = { this.diceRolling }
+         />
+         <Rolls
+         rollingLog = { this.state.previousLog }
          />
       </>
     )
--- props-chrissie-ruben/src/components/Rolls.js
+++ props-chrissie-ruben/src/components/Rolls.js
@@ -2,9 +2,15 @@ import React, {Component} from 'react'
 
 class Rolls extends Component {
     render() {
+        // let eachItem = this.props.rollingLog.map(value => {
+        //     return (<li> {value} </li>)
+        // })
+   
         return(
             <>
-            
+            <ul> 
+                { this.props.rollingLog }
+            </ul>
             </>
         )
     }
