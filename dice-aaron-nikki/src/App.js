615f137e308caaa8d6f065b2b80e79a598e493be
aaron masanes <aaronmasanes@hotmail.com>
2020-05-12 13:50:42 -0700

    update

--- dice-aaron-nikki/src/App.js
+++ dice-aaron-nikki/src/App.js
@@ -3,41 +3,66 @@ import DiceRoller from './DiceRoller'
 import Logged from './Logged'
 import './App.css'
 
-//create a box that outputs a number
-//everytime we click on box it randomizes a number
-//create a random method inside of DicerRoller.js 
-//create another box for Logged JS which will log all previous dice rolls into Logged
-//create a restart button which will clear all info inside of log
-
 
 export default class App extends Component {
 constructor(props) {
   super(props)
   this.state = {
     dice: '',
-    logArray: []
+    logArray: [],
+   // diceImages: [dice1, dice2, dice3, dice4, dice5, dice6]
   }
 }
 
 randomNumber = () => {
   let random = Math.ceil(Math.random() * 6)
-  this.state.logArray.push(random)
+  this.state.logArray.push(random + ',' + ' ')
   this.setState({ dice: random, logArray: this.state.logArray })
   
-
 }
 
-
+restart = () => {
+  this.setState({logArray: []}) //clears the log of numbers
+  this.setState({dice: ''}) //clear the currently rolled number
+}
 
   render() {
   return (
-    <> 
-            <h1>Dice Roller</h1>
+    <div> 
+      <h3 style={{
+        textAlign: 'center',
+        cursor: 'grab',
+        border: '1px solid black',
+        background: 'Yellow',
+        padding: '0.5rem 2rem',
+        margin: '2.5rem',
+        width: 'auto'
+               }}
+      >Dice Roller</h3>
+
       <DiceRoller dice={this.state.dice}/>
       <Logged log={this.state.logArray}/>
-      <button onClick={ this.randomNumber }>Generate a random number</button> 
-      
-    </>
+      <button onClick={ this.randomNumber } style={{
+        font: 'inherit',
+        cursor: 'pointer',
+        border: '1px solid red',
+        background: 'red',
+        padding: '0.5rem 2rem',
+        margin: '2rem'
+      }}
+      >ROLL THE DICE!!</button> 
+      <button onClick={ this.restart }
+      style={{
+        font: 'inherit',
+        cursor: 'pointer',
+        border: '1px solid red',
+        background: 'blue',
+        padding: '0.5rem 2rem',
+        margin: '2.5rem',
+        
+      }}
+      >DELETE DICE COUNT</button> 
+    </div>
   )
 }
 }
