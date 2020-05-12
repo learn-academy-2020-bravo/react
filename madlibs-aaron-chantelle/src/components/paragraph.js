280b435311e89158da76fbcf581b20ac49cb9e80
aaron masanes <aaronmasanes@hotmail.com>
2020-05-12 16:46:57 -0700

    functional

--- madlibs-aaron-chantelle/src/components/paragraph.js
+++ madlibs-aaron-chantelle/src/components/paragraph.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react'
 const Paragraph = (props) => {
         return (
             <div>
-              <p>Once upon a time, there was a { props.noun }. This { props.noun } was very [adjective] and loved to play [sport]. When it was [weather] outside, he would paint his [number] [bodypart-plural]. Every time the mail came, he would [verb] outside and [verb] open the envelope. Then he would [verb] the letter.</p>   
-              <p> testing testing { props.noun } </p>
+              <p>Once upon a time, there was a { props.form.noun }. This { props.form.noun } was very {props.form.adj} and loved to play {props.form.sport}. When it was {props.form.weather} outside, he would paint his {props.form.number} {props.form.bodyPlural}. Every time the mail came, he would {props.form.verb1} outside and {props.form.verb2} open the envelope. Then he would {props.form.verb3} the letter.</p>   
+
             </div>
         )
     }
