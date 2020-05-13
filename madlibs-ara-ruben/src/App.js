d730404c991cfcb189d70f7824aa3d2873efb586
Ara Jo <arajodeveloper@gmail.com>
2020-05-12 17:14:13 -0700

    now clear button works

--- madlibs-ara-ruben/src/App.js
+++ madlibs-ara-ruben/src/App.js
@@ -6,32 +6,31 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      madlibs:{
-        location: "",
-        famousPerson: "",
-        adjective:"",
-        sound:"",
-        animal1:"",
-        animal2:"",
-      },
+      location: "",
+      famousPerson: "",
+      adjective:"",
+      sound:"",
+      animal1:"",
+      animal2:"",
       paragraph: ''
     }
   }
 
   handleChange = (e) => {
-    const {madlibs} = this.state
-    madlibs[e.target.name] = e.target.value
-    // this.setState({e.target.name: e.target.value})
+    this.setState({[e.target.name]: e.target.value})
   }
 
   handleFormSubmit = (e) => {
-    var paragraph = `just waking up in the ${this.state.madlibs.location}, gotta thank ${this.state.madlibs.famousPerson}
-    I don't know, but today seems kinda ${this.state.madlibs.adjective}. No ${this.state.madlibs.sound} from the ${this.state.madlibs.animal1}, no smog, and momma cooked a breakfast with no ${this.state.madlibs.animal2}`
+    e.preventDefault()
+    var paragraph = `just waking up in ${this.state.location}, gotta thank ${this.state.famousPerson}
+    I don't know, but today seems kinda ${this.state.adjective}. No ${this.state.sound} from the ${this.state.animal1}, no smog, and momma cooked a breakfast with no ${this.state.animal2}`
     this.setState({paragraph: paragraph})
   }
 
-  handleClear = (e) => {
-    this.setState({paragraph: this.state.madlibs.paragraph, location:"", famousPerson:"", adjective: "", sound:"", animal1:"", animal2:""})
+  handleClear = () => {
+    console.log("hi!");
+
+    this.setState({paragraph: this.state.paragraph, location:"", famousPerson:"", adjective: "", sound:"", animal1:"", animal2:""})
   }
 
 
@@ -40,52 +39,51 @@ class App extends Component {
     return (
       <>
 	  <div className="flex-container">
-	  	<div >
-		  
+	  	<div className="items" >
+
 			<label>Location</label>
 			<input
 			value={ this.state.location }
 			name={ "location" }
 			onChange={ this.handleChange }></input>
-			<br></br>
+			</div><div className="items">
 			<label>Famous Person</label>
 			<input
 			value={ this.state.famousPerson }
 			name={ "famousPerson" }
 			onChange={ this.handleChange }></input>
-			<br></br>
+      </div><div className="items">
 				<label>Adjective</label>
 			<input
 			value={ this.state.adjective }
 			name={ "adjective" }
 			onChange={ this.handleChange }></input>
-			<br></br>
+			</div><div className="items">
 			<label>Sound</label>
 			<input
 			value={ this.state.sound }
 			name={ "sound" }
 			onChange={ this.handleChange }></input>
-			<br></br>
+		  </div><div className="items">
 			<label>Animal #1</label>
 			<input
 			value={ this.state.animal1 }
 			name={ "animal1" }
 			onChange={ this.handleChange }></input>
-			<br></br>
+			</div><div className="items">
 			<label>Animal #2</label>
 			<input
 			value={ this.state.animal2 }
 			name={ "animal2" }
 			onChange={ this.handleChange }></input>
-			<br></br>
+			</div><div className="items">
 			<button type="submit" onClick={this.handleFormSubmit}>Submit</button>
 			<button type="clear" onClick={this.handleClear}>Clear</button>
 	  </div>
+    </div>
 	  	<div >
-		  
-      		<Story paragraph={this.state.paragraph} />
+    	<Story paragraph={this.state.paragraph} />
 	  	</div>
-	   </div>
       </>
 
     );
