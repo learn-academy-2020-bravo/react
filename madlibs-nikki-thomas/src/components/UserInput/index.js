a73beff0d2fa69f72902b33894f023f2571f105f
Thomas Gonda <passinto06@gmail.com>
2020-05-13 03:21:53 -0700

    css stylings

--- madlibs-nikki-thomas/src/components/UserInput/index.js
+++ madlibs-nikki-thomas/src/components/UserInput/index.js
@@ -4,7 +4,8 @@ class UserInput extends Component {
 
   render() {
     return (
-      <input
+      //the button has a function attached to it that will run whenever the input field is changed.
+      <input className = "inputField"
         onChange={ this.props.handleChange }
         name = {this.props.fieldName}
       />
