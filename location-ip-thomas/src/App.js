b0940e00d2dc4314908d773fbf35f74bf91f47aa
Thomas Gonda <passinto06@gmail.com>
2020-06-05 15:36:20 -0700

    fixed how states are generated

--- location-ip-thomas/src/App.js
+++ location-ip-thomas/src/App.js
@@ -6,17 +6,16 @@ class App extends Component{
   constructor(){
     super()
     this.state = {
-      payload: "", // Articles is an empty array to store the parsed payload,
+      location: "",
       ip: "",
       error: null // If we have an error from the API return it here
     }
   }
 
-  getLocation = () => {
-    if (this.state.ip.length === 0) {
+  componentDidMount() {
+     //find IP address
       let findIP = `https://ipapi.co/ip/`
       fetch(findIP)
-
       .then((response) => {
         if(response.status !== 200){
           throw({ message: "Could not perform search. Please try again." })
@@ -29,26 +28,24 @@ class App extends Component{
        })
       .catch((error) => this.setState({error}))
 
+      //find location based on IP address.
        let searchUrl = `https://ipapi.co/${this.state.ip}/json/`
-
        fetch(searchUrl)
        .then((response) => {
          if(response.status !== 200){
            throw({ message: "Could not perform search. Please try again." })
-         } // allowing for error messages
-         return response.json() // Return the response as JSON
+         }
+         return response.json()
        })
         .then((payload) => {
          let location = `${payload.city}, ${payload.region}`
-         //console.log(Object.values(articles))
          this.setState({ location: location })
        })
        .catch((error) => this.setState({error}))
-     }
+
   }
 
   render(){
-    { this.getLocation() }
     return(
       <React.Fragment>
 
